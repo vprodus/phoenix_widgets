67dcab0f1fcbd136cbbd854c74a93e15ca942c41
Vadim Vprodus <vproduspro@gmail.com>
2024-02-07 00:45:14 +0300

    added fonts and lucide icons

--- assets/tailwind.config.js
+++ assets/tailwind.config.js
@@ -14,10 +14,57 @@ module.exports = {
   ],
   theme: {
     extend: {
+      container: {
+        center: true,
+        padding: {
+          "DEFAULT": "1rem",
+          "sm": "2rem",
+          "lg": "4rem",
+          "xl": "6rem",
+          "2xl": "8rem",
+        },
+      },
       fontFamily: {
+        body: ["'DM Sans'", "sans-serif"],
         poppins: ["Poppins", "sans-serif"],
       },
     },
+  },
+  daisyui: {
+    themes: [
+      {
+        light: {
+          ...require("daisyui/src/theming/themes").light,
+          "primary": "#1b77ff",
+          "primary-content": "#ffffff",
+          "secondary": "#494949",
+          "neutral": "#03131a",
+          "info": "#00e1ff",
+          "success": "#90ca27",
+          "warning": "#ff8800",
+          "error": "#ff7f7f",
+          "--rounded-box": "0.25rem",
+          "--rounded-btn": "0.25rem",
+        },
+        dark: {
+          ...require("daisyui/src/theming/themes").dark,
+          "primary": "#1b77ff",
+          "primary-content": "#ffffff",
+          "secondary": "#494949",
+          "neutral": "#03131a",
+          "info": "#00e1ff",
+          "success": "#90ca27",
+          "warning": "#ff8800",
+          "error": "#ff7f7f",
+          "base-100": "#14181c",
+          "base-200": "#1e2328",
+          "base-300": "#28323c",
+          "base-content": "#dcebfa",
+          "--rounded-box": "0.25rem",
+          "--rounded-btn": "0.25rem",
+        },
+      },
+    ],
   },
   plugins: [
     require("@tailwindcss/forms"),
